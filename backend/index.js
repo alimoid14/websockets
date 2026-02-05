f102a9c3f7d27df8ab8705109368d49f25cf636d
alimoid14 <syedalimoidrizvi@gmail.com>
2026-02-05 16:51:32 +0530

    Add db schema and incorporate zod for input validation before db insertion

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
 import express from "express";
-
+import { matchRouter } from "./routes/matches.js";
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); //read JSON bodies in requests and attach them to req.body
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello, World!");
+// });
+
+app.use("/matches", matchRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
